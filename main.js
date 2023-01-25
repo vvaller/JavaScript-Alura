6b9fb22690b60fa0a344992b4bce1e37a67da87c
Vinicius <viniciusvaller@gmail.com>
2023-01-24 22:23:24 -0300

    Resultado final do curso

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
 function tocaSom(idElementoAudio) {
 
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
 
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Valor inválido')
+    }
 };
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -11,10 +16,19 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumentos = tecla.classList[1];
-    
+
     const idAudio = `#som_${instrumentos}`; // template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
-    }; 
+    };
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 };
